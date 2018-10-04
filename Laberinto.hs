8f9d73b4848c364471abd30927b1a07f0e427096
Christian Oliveros <christianol_01@hotmail.com>
2018-10-03 20:50:01 -0400

    Added Laberinto Test Functions and some Haddock comments

--- Laberinto.hs
+++ Laberinto.hs
@@ -1,15 +1,50 @@
-module Laberinto () where
+{-|
+Module      : Laberinto
+Description : Modulo para manejar un Laberinto
+Copyright   : (c) Christian Oliveros, 2018
+                  David Rodriguez, 2018
+License     : MIT
+Maintainer  : sample@email.com
+Stability   : experimental
+
+Módulo para crear y manejar un Laberinto.
+-}
+module Laberinto (
+  Laberinto,
+  laberintoStart, 
+  tesoroAdd
+) where
 
 import Data.Maybe
 import Data.String
 import Text.Read
 
+-- | Conocimientos sobre un Laberinto
+data Laberinto 
+  -- | Tesoro oculto en un Laberinto
+  = Tesoro {  -- | Descripción de un tesoro
+              desc :: String
+            -- | Laberinto indicando qué encontrarán si pasan por alto el tesoro
+            , fwd :: Maybe Laberinto 
+            }
+  -- | Trifurcacion del Laberinto
+  | Trifurcacion { -- | Laberinto indicando lo alcanzable al voltear a la izquierda, 
+                   -- | o Nothing si no es posible voltear a la izquierda
+                    lft :: Maybe Laberinto
+                  -- | Laberinto indicando lo alcanzable al voltear a la derecha, 
+                  -- | o Nothing si no es posible voltear a la derecha
+                  , fwd :: Maybe Laberinto
+                  -- | Laberinto indicando lo alcanzable al seguir a la recto, 
+                  -- | o Nothing si no es posible seguir a la recto
+                  , rgt :: Maybe Laberinto
+                  }
+    deriving (Show, Read)
 
-data Laberinto = Tesoro { desc :: String
-                        , fwd :: Maybe Laberinto 
-                        }
-                | Trifurcacion { lft :: Maybe Laberinto
-                               , fwd :: Maybe Laberinto
-                               , rgt :: Maybe Laberinto
-                               }
-                deriving (Show, Read)
+-- | Función que genera una Trifurcación sin salida
+laberintoStart :: Laberinto
+laberintoStart = Trifurcacion Nothing Nothing Nothing
+
+-- | Crea un tesoro, con su descripción y un Laberinto 
+-- | indicando qué encontrarán si pasan por alto el tesoro
+tesoroAdd :: String -> Laberinto -> Laberinto
+tesoroAdd d l = Tesoro d (Just l)
