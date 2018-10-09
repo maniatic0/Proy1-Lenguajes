b9110b165bf7ba4adb716e4ae6d027ce461bc582
Christian Oliveros <christianol_01@hotmail.com>
2018-10-08 23:33:51 -0400

    Fixed some message errorrs. Added laberintoNew

--- src/Sabio.hs
+++ src/Sabio.hs
@@ -11,6 +11,8 @@ Módulo para que un Sabio maneje un Laberinto.
 -}
 module Main (
     main,
+    -- * Manejo de Sabio
+    laberintoNew,
     -- * Lectura de Rutas
     RutaState,
     getRuta,
@@ -24,9 +26,9 @@ import Laberinto
     
 main :: IO ()
 main = do
-    putStrLn "Hello, Haskell!"
-    ruta <- execStateT getRutaStart []
-    print ruta
+  putStrLn "Hello, Haskell!"
+  lab <- laberintoNew
+  print lab
 
 
 -- * Lectura de Rutas
@@ -38,30 +40,41 @@ type RutaState = StateT Ruta IO ()
 -- No Imprime Instrucciones de uso y devuelve la ruta al revéz
 getRuta :: RutaState
 getRuta = do 
-    linea <- lift getLine
-    case linea == "" of 
-        True -> lift $ putStr "Ruta Leida Completa: " 
-        False -> do 
-            case readMaybe linea :: Maybe Indicador of
-                Nothing -> do lift $ 
-                                putStrLn ("Error leyendo parte de Ruta. Input Invalido: " ++ linea)
-                              getRuta
-                Just x -> do modify (\xs -> x:xs)
-                             r <- get
-                             lift $ putStrLn ("Ruta actual: " ++ show r)
-                             getRuta
+  linea <- lift getLine
+  case linea == "" of 
+    True -> lift $ putStr "Ruta Leida Completa: " 
+    False -> do 
+      case readMaybe linea :: Maybe Indicador of
+        Nothing -> do lift $ 
+                        putStrLn ("Error leyendo parte de Ruta. Input Invalido: " ++ linea)
+                      getRuta
+        Just x -> do modify (\xs -> x:xs)
+                     r <- get
+                     lift $ putStrLn ("Ruta actual invertida: " ++ show r)
+                     getRuta
 
 -- | Manejo de Lectura de una Ruta. 
 -- Imprime Instrucciones de uso y devuelve la ruta de forma correcta
 getRutaStart :: RutaState
 getRutaStart = do 
-    lift $ print linea
-    getRuta
-    modify reverse
-    r <- get 
-    lift $ print r
-    where linea = "Escriba 'Izq', 'Der' o 'Rect' para indicar una seccion de la ruta.\
-                \ Luego presione enter para insertarlo en la ruta.\n \
-                \Para indicar fin de ruta, envie una linea vacía.\n \
-                \La Lista mostrada esta en orden inverso al mostrado"
-                
+  lift $ putStrLn msg
+  getRuta
+  modify reverse
+  r <- get 
+  lift $ print r
+  where msg = "Escriba 'Izq', 'Der' o 'Rect' para indicar una seccion de la ruta.\
+              \ Luego presione enter para insertarlo en la ruta.\n\
+              \Para indicar fin de ruta, envie una linea vacía.\n\
+              \La Lista mostrada esta en orden inverso al insertado"
+ 
+-- * Manejo de Sabio
+
+-- | Comenzar a hablar de un laberinto nuevo: Si esta opción es seleccionada, 
+-- se reemplaza el laberinto en memoria con un laberinto vacío y se pide una ruta 
+-- que puede ser seguida en el mismo. Se usa esta ruta para poblar el laberinto inicialmente. 
+-- La ruta debe suponerse terminada en un camino sin salida y no contener tesoros.
+laberintoNew :: IO Laberinto
+laberintoNew = do
+  putStrLn "Comenzando Laberinto nuevo.\nPor favor indique Ruta inicial."
+  ruta <- execStateT getRutaStart []
+  return (foldr (\i l-> trifurcacionAdd laberintoStart l i) laberintoStart ruta)
