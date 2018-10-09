d818d34898a169f457d73bf7a130db87f9845034
Christian Oliveros <christianol_01@hotmail.com>
2018-10-08 21:55:54 -0400

    Started State IO Monads

--- src/Sabio.hs
+++ src/Sabio.hs
@@ -11,7 +11,46 @@ Módulo para que un Sabio maneje un Laberinto.
 -}
 module Main where
 
+import Control.Monad.State 
+import Text.Read hiding (lift, get)
+
 import Laberinto
     
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    putStrLn "Hello, Haskell!"
+    ruta <- execStateT getRutaStart []
+    putStrLn $ show ruta
+
+-- | Monad State para manejar lectura de una ruta
+type RutaState = StateT Ruta IO ()
+
+
+getRuta :: RutaState
+getRuta = do 
+    linea <- lift getLine
+    case linea == "" of 
+        True -> lift $ putStr "Ruta Leida Completa: " 
+        False -> do 
+            case (readMaybe linea) :: Maybe Indicador of
+                Nothing -> do 
+                    lift $ putStrLn ("Error leyendo parte de Ruta. Input Invalido: " ++ linea)
+                    getRuta
+                Just x -> do 
+                    modify (\xs -> xs ++ [x])
+                    r <- get
+                    lift $ putStrLn ("Ruta actual: " ++ show r)
+                    getRuta
+
+getRutaStart :: RutaState
+getRutaStart = do 
+    lift $ putStrLn linea
+    getRuta
+    modify reverse
+    r <- get 
+    lift $ putStrLn (show r)
+    where linea = "Escriba 'Izq', 'Der' o 'Rect' para indicar una seccion de la ruta.\
+                \ Luego presione enter para insertarlo en la ruta.\n \
+                \Para indicar fin de ruta, envie una linea vacía.\n \
+                \La Lista mostrada esta en orden inverso al mostrado"
+                
